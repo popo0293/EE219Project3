d241bfc5de3e33ea1735b81d944f8e024cafa1d5
Yichen Wu <wyc20020111@gmail.com>
2018-02-19 20:43:49 -0800

    q6

--- project3.py
+++ project3.py
@@ -3,27 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load data
-data = pd.read_csv('data/ratings.csv', header=0, usecols=[0, 1, 2])
-print(data.head())
-R = data.pivot_table(index='userId', columns='movieId', values='rating', fill_value=0).values
-
+r_data = pd.read_csv('data/ratings.csv', header=0, usecols=[0, 1, 2])
+print(r_data.head())
+R = r_data.pivot_table(index='userId', columns='movieId', values='rating', fill_value=0).values
 print("(number of users, number of rated movies): ", R.shape)
 
 # Question 1
 user_count = R.shape[0]
 movie_count = R.shape[1]
 max_rating_count = user_count*movie_count
-rating_count = data.size/3
+rating_count = r_data.size/3
 sparsity = rating_count*1.0/max_rating_count
 print("Matrix sparsity = %0.4f" % sparsity)
 
 # Question 2
 plt.figure()
 ax = plt.subplot(111)
-ratings = data.filter(items=['rating'])
+ratings = r_data.rating.tolist()
 xrange = np.arange(0, 5.5, 0.5)
-ax.hist(ratings.values, bins=xrange)
+ax.hist(ratings, bins=xrange)
 ax.set_xticks(xrange)
+ax.title("Frequency of rating values")
+ax.set_xlabel("rating values")
+ax.set_ylabel("movie count")
 plt.show()
 
 # Question 3
@@ -32,6 +34,9 @@ movie_rating_count = np.count_nonzero(R, axis=0)
 sorted_mrc = sorted(movie_rating_count, reverse=True)
 ax = plt.subplot(111)
 ax.plot(range(len(movie_rating_count)), sorted_mrc, '-')
+ax.title("Distribution of ratings among movies")
+ax.set_xlabel("most reviewed to least reviewed")
+ax.set_ylabel("rating count")
 plt.show()
 
 # Question 4
@@ -40,6 +45,9 @@ user_rating_count = np.count_nonzero(R, axis=1)
 sorted_urc = sorted(user_rating_count, reverse=True)
 ax = plt.subplot(111)
 ax.plot(range(len(user_rating_count)), sorted_urc, '-')
+ax.title("Distribution of ratings among users")
+ax.set_xlabel("most reviewed to least reviewed")
+ax.set_ylabel("rating count")
 plt.show()
 
 # Question 5
