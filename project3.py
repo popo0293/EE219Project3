3ab0f48c0181d77f52dc07e0a3233a536f6a6fe9
Yichen Wu <wyc20020111@gmail.com>
2018-02-19 17:38:05 -0800

    q1

--- project3.py
+++ project3.py
@@ -8,3 +8,13 @@ print(data.head())
 R = data.pivot_table(index='userId', columns='movieId', values='rating', fill_value=0).values
 
 print("(number of users, number of rated movies): ", R.shape)
+
+# Question 1
+user_count = R.shape[0]
+movie_count = R.shape[1]
+max_rating_count = user_count*movie_count
+rating_count = data.size/3
+sparsity = rating_count*1.0/max_rating_count
+print("Matrix sparsity = %0.4f" % sparsity)
+
+# Question 2
